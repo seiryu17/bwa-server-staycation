70a5a525f713fc725f63839a837ca226d77bd9b2
unknown <gulivverio@gmail.com>
2020-05-24 12:24:27 +0700

    full project staycation

---
+++ routes/admin.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const adminController = require('../controllers/adminController');
+const { uploadSingle } = require('../middlewares/multer');
+const { uploadMultiple } = require('../middlewares/multer');
+const auth = require('../middlewares/auth');
+
+router.get('/signin', adminController.viewSignin);
+router.post('/signin', adminController.actionSignin);
+router.use(auth);
+router.get('/logout', adminController.actionLogout);
+router.get('/dashboard', adminController.viewDashboard);
+router.get('/category', adminController.viewCategory);
+router.post('/category', adminController.addCategory);
+router.put('/category', adminController.editCategory);
+router.delete('/category/:id', adminController.deleteCategory);
+router.get('/bank', adminController.viewBank);
+router.post('/bank', uploadSingle, adminController.addBank);
+router.put('/bank', uploadSingle, adminController.editBank);
+router.delete('/bank/:id', adminController.deleteBank);
+router.get('/item', adminController.viewItem);
+router.post('/item', uploadMultiple, adminController.addItem);
+router.get('/item/show-image/:id', adminController.showImage);
+router.get('/item/:id', adminController.showEditItem);
+router.put('/item/:id', uploadMultiple, adminController.editItem);
+router.delete('/item/:id/delete', adminController.deleteItem);
+router.get('/item/show-detail-item/:itemId', adminController.showDetailItem);
+router.post('/item/add-feature', uploadSingle, adminController.addFeature);
+router.put('/item/update/feature', uploadSingle, adminController.updateFeature);
+router.delete('/item/:itemId/feature/:id', adminController.deleteFeature);
+router.post('/item/add-activity', uploadSingle, adminController.addActivity);
+router.put('/item/update/activity', uploadSingle, adminController.updateActivity);
+router.delete('/item/:itemId/activity/:id', adminController.deleteActivity);
+router.get('/booking', adminController.viewBooking);
+router.get('/booking/:id', adminController.showDetailBooking);
+router.put('/booking/:id/accept', adminController.acceptBooking);
+router.put('/booking/:id/reject', adminController.rejectBooking);
+
+module.exports = router;
